759ece0a2fc7dc49251f57586d65231ff467512f
vespino <6991325+vespino@users.noreply.github.com>
2024-12-12 16:56:44 +0100

    Update DebugHandler.php
    Take care of "Creation of dynamic property is deprecated" error

--- src/DebugHandler.php
+++ src/DebugHandler.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace tuyapiphp;
+	namespace tuyapiphp;
 
-class DebugHandler
-{
-    public function __construct(protected array $_config)
-    {
-    }
-
-    public function output($msg, $data = null)
-    {
-        if (@$this->_config['debug'] != true) {
-            return;
-        }
-        if ($data) {
-            echo $msg;
-            echo '<pre>'.print_r($data, true).'</pre>';
-
-            return;
-        }
-        echo $msg."<br>\n";
-    }
-}
+	Class DebugHandler
+	{
+		public $_config;
+		
+		public function __construct($config)
+		{
+			$this->_config = $config;
+		}
+	
+		public function output($msg, $data = null)
+		{
+			if (@$this->_config['debug'] != true){ return; }
+			if ($data)
+			{
+				echo $msg;
+				echo "<pre>" . print_r($data, true) . "</pre>";
+				return;
+			}
+			echo $msg . "<br>\n";
+		}
+	}
